aa31f8c61afa6d158700568dbb0bfbf214cc5de1
ArnulfoAguilar <arnulfo.aguilar9405@gmail.com>
2018-06-13 23:33:00 -0600

    no se que es esto

--- resources/views/vendor/adminlte/errors/404.blade.php
+++ resources/views/vendor/adminlte/errors/404.blade.php
@@ -9,10 +9,9 @@
     <div class="error-page">
         <h2 class="headline text-yellow"> 404</h2>
         <div class="error-content">
-            <h3><i class="fa fa-warning text-yellow"></i> Oops! {{ trans('adminlte_lang::message.pagenotfound') }}.</h3>
+            <h3><i class="fa fa-warning text-yellow"></i> Oops! No encontramos esta pagina.</h3>
             <p>
-                {{ trans('adminlte_lang::message.notfindpage') }}
-                {{ trans('adminlte_lang::message.mainwhile') }} <a href='{{ url('/home') }}'>{{ trans('adminlte_lang::message.returndashboard') }}</a> {{ trans('adminlte_lang::message.usingsearch') }}
+                Mejor ve a Home y revisa bien la ruta <a href='{{ url('/home') }}'> Home</a> {{ 
             </p>
             <form class='search-form'>
                 <div class='input-group'>
