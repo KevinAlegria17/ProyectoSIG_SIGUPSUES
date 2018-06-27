0298d52eac1cbba1c7829c220d4d430ff3f06734
Arnulfo trabajo <arnulfo.aguilar9405@gmail.com>
2018-06-27 13:17:24 -0600

    Home

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -3,34 +3,21 @@
 @section('htmlheader_title','HOME')
 
 
-@section('contentheader_title', 'Inicio')
+@section('contentheader_title', '')
 
 @section('main-content')
 	
 <div class="container-fluid">
-  <div class="row">
-  <div class="col-md-9">
-    <div class="card">
-        <div class="card-header">PANEL PRINCIPAL</div>
-
-          <div class="card-body">
-              @if (session('status'))
-              <div class="alert alert-success">
-                {{ session('status') }}
-              </div>
-              @endif
+  
 
               <h1>Bienvenido {{ Auth::user()->name }}!</h1>
               <br></br>
-          </div>
-       </div>
-      </div>
-  </div>
-  </div>
+        
 
   <div class="row">
     
     <div class="info-box-content col-md-4">
+      <a style="text-decoration: none; color:black;" href="RepoNoEscogido">
               <span class="info-box-text">Proyectos No Escogidos</span>
               <span class="info-box-number">{{$noEscogido}}</span>
 
@@ -38,25 +25,31 @@
                 <div class="progress-bar" style="width: {{$noEscogido}}%"></div>
               </div>
               <span class="progress-description">
-                    {{$noEscogido}}% Increase in 30 Days
+                    {{$noEscogido}}% Incrementado en los ultimos 30 Dias
                   </span>
+      </a>
     </div>
 
 
             <div class="info-box-content col-md-4">
-              <span class="info-box-text">Abandonados</span>
+              <a style="text-decoration: none; color:black;" href="RepoAbandonados">
+              <span class="info-box-text">Proyectos Abandonados</span>
               <span class="info-box-number">{{$Abandonados}}</span>
 
               <div class="progress">
                 <div class="progress-bar" style="width: {{$Abandonados}}%"></div>
               </div>
               <span class="progress-description">
-                    {{$Abandonados}}% Increase in 30 Days
+                    {{$Abandonados}}% Incrementado en los ultimos 30 Dias
                   </span>
+                </a>
             </div>
-
+</div>
+<br>
+<div class="row">
 
             <div class="info-box-content col-md-4">
+              <a style="text-decoration: none; color:black;" href="cantidadBeneficiarios">
               <span class="info-box-text">Cantidad Beneficiarios</span>
               <span class="info-box-number">{{$CantidadBeneficiarios}}</span>
 
@@ -64,24 +57,30 @@
                 <div class="progress-bar" style="width: {{$CantidadBeneficiarios}}%"></div>
               </div>
               <span class="progress-description">
-                    {{$CantidadBeneficiarios}}% Increase in 30 Days
+                    {{$CantidadBeneficiarios}}% Incrementado en los ultimos 30 Dias
                   </span>
+                </a>
             </div>
 
 
             <div class="info-box-content col-md-4">
-              <span class="info-box-text">Cupos</span>
+              <a style="text-decoration: none; color:black;" href="cuposDisponibles">
+              <span class="info-box-text">Cupos Disponibles</span>
               <span class="info-box-number">{{$Cupos}}</span>
 
               <div class="progress">
                 <div class="progress-bar" style="width: {{$Cupos}}%"></div>
               </div>
               <span class="progress-description">
-                    {{$Cupos}}% Increase in 30 Days
+                    {{$Cupos}}% Incrementado en los ultimos 30 Dias
                   </span>
+                </a>
             </div>
-
+</div>
+<br/>
+<div class="row">
             <div class="info-box-content col-md-4">
+              <a style="text-decoration: none; color:black; color:black;" href="{{ route('peticiones') }}" style="text-decoration: none; color:black;">
               <span class="info-box-text">Cantidad de Peticiones</span>
               <span class="info-box-number">{{$CantidadPeticiones}}</span>
 
@@ -91,20 +90,24 @@
               <span class="progress-description">
                     {{$CantidadPeticiones}}% Increase in 30 Days
                   </span>
+                </a>
             </div>
 
             <div class="info-box-content col-md-4">
-              <span class="info-box-text">dinero Ahorrado</span>
+              <a style="text-decoration: none; color:black;" href="/dineroAhorrado">
+              <span class="info-box-text">Dinero Ahorrado</span>
               <span class="info-box-number">{{$dineroAhorrado}}</span>
 
               <div class="progress">
                 <div class="progress-bar" style="width: {{$dineroAhorrado}}%"></div>
               </div>
               <span class="progress-description">
-                    {{$dineroAhorrado}}% Increase in 30 Days
+                    {{$dineroAhorrado}}% Incrementado en los ultimos 30 Dias
                   </span>
+                </a>
             </div>
   </div>
+
 </div>
 
 
